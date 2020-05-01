01d6322482bc57976e1b181df91b2c536c383417
a.diachenko <alexdgx@gmail.com>
2020-05-01 17:35:12 +0300

    refactoring configuration in DAL

--- Students/DAL/Context/StudentsContext.cs
+++ Students/DAL/Context/StudentsContext.cs
@@ -2,7 +2,11 @@ namespace DAL.Context
 {
     using DAL.Configuratons;
     using Models;
+    using System;
     using System.Data.Entity;
+    using System.Data.Entity.ModelConfiguration;
+    using System.Linq;
+    using System.Reflection;
 
     public class StudentsContext : DbContext
     {
@@ -24,13 +28,26 @@ namespace DAL.Context
         public virtual DbSet<Schedule> Schedules { get; set; }
         public virtual DbSet<Course> Courses { get; set; }
 
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+
+        //    modelBuilder.Configurations.Add(new StudentConfig());
+        //}
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            var typesToRegister = Assembly.GetExecutingAssembly().GetTypes()
+             .Where(type => !String.IsNullOrEmpty(type.Namespace))
+             .Where(type => type.BaseType != null && type.BaseType.IsGenericType
+                  && type.BaseType.GetGenericTypeDefinition() == typeof(EntityTypeConfiguration<>));
+            foreach (var type in typesToRegister)
+            {
+                dynamic configurationInstance = Activator.CreateInstance(type);
+                modelBuilder.Configurations.Add(configurationInstance);
+            }
             base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Configurations.Add(new StudentConfig());
         }
-
     }
 
 }
